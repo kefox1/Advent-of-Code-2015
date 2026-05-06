c19ee12f18fe0d06cc310af18fefa030b652b75e
kefox1 <kefox2727@gmail.com>
2026-05-06 21:25:05 +0930

    day 4 part 2

--- solutions/cs/day04.cs
+++ solutions/cs/day04.cs
@@ -10,18 +10,20 @@ namespace adventofcode2015.day04
         {
             foreach (string input in File.ReadLines("inputs/day04.txt"))
             {
-                int key = CalculateHash(input);
-                Console.WriteLine("Number: "+key);
+                int fiveDigitNum = CalculateHash(input,5);
+                int sixDigitNum = CalculateHash(input,6);
+                Console.WriteLine("Five Digit Number: " + fiveDigitNum);
+                Console.WriteLine("Six Digit Number: " + sixDigitNum);
             }
         }
 
-        static int CalculateHash(string input)
+        static int CalculateHash(string input, int length)
         {
             int suffixNum = 0;
             while (true)
             {
                 string hash = CreateMD5(input+suffixNum.ToString());
-                if (hash.Substring(0,5) == "00000")
+                if (hash.Substring(0,length) == new string('0',length))
                 {
                     return suffixNum;
                 }
